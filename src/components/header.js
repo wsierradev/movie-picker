a18edec58bc84000f04259ae16648f1e68b63748
William Sierra <tealsasuke@aol.com>
2021-03-13 14:56:12 -0500

    Added Categories to Navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,24 @@
-import React from "react"
-import { Navbar, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Link } from "gatsby"
-
-
-
+import React, { useState,useEffect } from 'react';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "gatsby";
+import axios from "axios";
 
 export default function Header() {
+  const [categories, setCategories] = useState([1,2,3,4,5]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const res = await axios(
+        'https://random-movie-picker-kazan.herokuapp.com/categories',
+      );
+
+      setCategories(res.data);
+    };
+ 
+    fetchCategories();
+  }, []);
+
   return (
     <header>
     <Navbar bg="light" expand="lg">
@@ -16,6 +28,15 @@ export default function Header() {
         <Nav className="mr-auto">
           <Nav.Item as="li"><Link to="/random/" className="nav-link">Random Movie</Link></Nav.Item>
           <Nav.Item as="li"><Link to="/list/" className="nav-link">Movie List</Link></Nav.Item>
+        <NavDropdown title="Categories" id="basic-nav-dropdown">
+        {categories.map((category) => 
+  <NavDropdown.Item as="li"><Link to= {`/${category.value}/`} className="nav-link">{category.display}</Link></NavDropdown.Item>
+   )}
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+
+          {/* {categoryList} */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
